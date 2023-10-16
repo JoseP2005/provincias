4688b887ce43b7531f8ffc7ee6ebda2eda569675
JoseP2005 <joseandres.pascualaviles@usp.ceu.es>
2023-10-16 12:02:01 +0200

    1.1

--- src/aplicacion/Principal.java
+++ src/aplicacion/Principal.java
@@ -1,1 +1,10 @@
-aaaaa
+package aplicacion;
+import dominio.*;
+public class Principal{
+		public static void main(String[] args) {
+			Localidad Madrid = new Localidad();
+			Madrid.Localidad();
+			
+			Localidad Localidad = new Localidad(Madrid);
+			}
+    }
--- src/dominio/Localidad.java
+++ src/dominio/Localidad.java
@@ -17,4 +17,12 @@ public class Localidad
     public String getnumeroDeHabitantes(){
         return numeroDeHabitantes;
     }
+    public String toString(){
+		String = España;
+		this.España}
+		
+    public Localidad(String nombre, String numeroDeHabitantes){
+        this.nombre = nombre;
+        this.numeroDeHabitantes = numeroDeHabitantes;
+    }
 }
